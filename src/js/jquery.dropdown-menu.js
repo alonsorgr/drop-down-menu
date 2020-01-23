086cfaa21149bf53647a07476419f712b3344de6
Alonso García Raposo <alonso.garcia@iesdonana.org>
2020-01-23 17:19:00 +0100

    Add plugin structure

--- src/js/jquery.dropdown-menu.js
+++ src/js/jquery.dropdown-menu.js
@@ -8,3 +8,37 @@
  * @author alonsorgr
  * @since  1.0.0
  */
+
+(function($) { 
+
+    var methods = [
+        addSection : function(section) {
+
+        },
+
+        delSection : function(name) {
+
+        },
+
+        addOption : function(option) {
+
+        },
+
+        delOption : function(option) {
+
+        },
+    ];
+
+    $.fn.tooltip = function( method ) {
+
+        if (methods[method]) {
+            return methods[method].apply( this, Array.prototype.slice.call(arguments,1 ));
+        } 
+        else if (typeof method === 'object' || ! method) {
+            return methods.init.apply( this, arguments );
+        } else {
+            alert('La función ' + method + ' no existe en jQuery.tooltip' );
+        }
+    };
+
+})(jQuery);
