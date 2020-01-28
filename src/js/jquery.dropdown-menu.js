57863553b107f257ca5fd955f132289f31f7c474
Alonso García <alonso.garcia@iesdonana.org>
2020-01-28 01:36:55 +0100

    POO

--- src/js/jquery.dropdown-menu.js
+++ src/js/jquery.dropdown-menu.js
@@ -1,141 +1,206 @@
 /**
  * DropDown Menu
+ * @since       1.0.0
+ * @access      private
+ * @memberof    jquery.dropdown-menu
  *
- * JQuery library for creating drop-down menus
- *
- * @link   https://github.com/alonsorgr/drop-down-menu
- * @file   This file defines the functionality of the JQuery plugin to create dynamic drop-down menus.
- * @author alonsorgr
- * @since  1.0.0
+ * @link        https://github.com/alonsorgr/drop-down-menu
+ * @file        This file defines the functionality of the JQuery plugin to create dynamic drop-down menus.
+ * @author      alonsorgr
+ * @since       1.0.0
  */
 
-(function ($) {
 
-    var counter = 0;
-
-    var methods = {
-        init: function (id) {
-
-            let nav = document.createElement('nav');
-            let navigation = document.createElement('ul');
-            nav.appendChild(navigation);
-
-            navigation.id = 'navigation';
-            navigation.setAttribute('class', 'navigation');
-
-            $(this).append(nav);
-        },
-
-        addSection: function (index, title) {
-            $('.navigation').append(document.createElement('ul').appendChild(new Section(index, title)));
-            _displayEvent('#header-'.concat(index), index);
-            $('#section-'.concat(index)).hide();
-        },
-
-        addSubSection: function (index, title) {
-            $('#item-'.concat(index)).append(document.createElement('ul').appendChild(new SubSection(index, title)));
-            _displayEvent('#header-'.concat(index), index);
-            $('#section-'.concat(index)).hide();
-        },
-
-        addItem: function (index, title, href) {
-            $('#section-'.concat(index)).append(new Item(counter++, title, href));
-        },
-    }
-
-    function _displayEvent(id, index) {
-        $(id).on({
-            mouseenter: function () {
-                $('#section-'.concat(index)).show();
-            },
-            mouseleave: function () {
-                $('#section-'.concat(index)).hide();
-            }
-        });
-    }
-
-    $.fn.dropDownMenu = function (method) {
-
-        if (methods[method]) {
-            return methods[method].apply(this, Array.prototype.slice.call(arguments, 1));
-        }
-        else if (typeof method === 'object' || !method) {
-            return methods.init.apply(this, arguments);
-        } else {
-            console.log('The function  ' + method + ' does not exist in jQuery.dropDownMenu');
-        }
-    };
-
-})(jQuery);
-
+ /**
+  * CSS classes constants
+  */
 const ITEM_CLASS = 'item';
-const LINK_CLASS = 'link';
+const SECTION_CLASS = 'section';
+const NAVBAR_CLASS = 'navbar';
 
+/**
+ * Item Counter
+ */
+var itemCounter = 0;
+
+/**
+ * Class Item
+ *
+ * @since       1.0.0
+ * @access      private
+ */
 class Item {
 
-    constructor(id, text, href) {
-        this._setId(id);
-        this._setText(text);
-        this._setHref(href);
-        this._setItem();
-        this._setLink();
+    /**
+     * Item constructor
+     * 
+     * @param {Integer} id 
+     * @param {String}  text 
+     * @param {String}  url 
+     */
+    constructor(id, text, url) {
+        this.item = document.createElement('a');
+        this.item.setAttribute('class', ITEM_CLASS);
+        this.item.id = 'item-'.concat(id);
+        this.item.text = text;
+        this.item.href = url;
         return this.item;
     }
-
-    _setId(id) {
-        this.id = id;
-    };
-
-    _setText(text) {
-        this.text = text;
-    }
-
-    _setHref(href) {
-        this.href = href;
-    }
-
-    _setItem(id) {
-        this.item = document.createElement('li');
-        this.item.setAttribute('class', ITEM_CLASS);
-        this.item.id = 'item-'.concat(this.id);
-    }
-
-    _setLink() {
-        let link = document.createElement('a');
-        link.setAttribute('class', LINK_CLASS);
-        link.id = 'link-'.concat(this.id);
-        link.href = this.href;
-        link.textContent = this.text;
-        this.item.appendChild(link);
-    }
 }
 
+/**
+ * Class Section
+ *
+ * @since       1.0.0
+ * @access      private
+ */
 class Section {
 
-    constructor(index, title) {
-        this.header = document.createElement('li');
-        this.section = document.createElement('div');
-        this.header.id = 'header-'.concat(index);
-        this.section.id = 'section-'.concat(index);
-        let text = document.createTextNode(title);
-        this.header.appendChild(text);
-        this.header.appendChild(this.section);
-        return this.header;
+    /**
+     * Section  constructor
+     * 
+     * @param {Integer} id 
+     */
+    constructor(id) {
+        this.menu = document.createElement('ul');
+        this.menu.setAttribute('class', SECTION_CLASS);
+        this.menu.id = 'menu-'.concat(id);
+        this.container = document.createElement('div');
+        this.elements = this.container.childNodes;
+        this._setMenu(this.menu, this.container);
+    }
+
+    _setMenu(menu, container) {
+        menu.appendChild(container);
+    }
+
+    /**
+     * Get menu instance
+     */
+    get() {
+        return this.menu;
+    }
+
+    /**
+     * Get all items of menu
+     */
+    getItems() {
+        return this.elements;
+    }
+
+    /**
+     * Add item to menu
+     * 
+     * @param {Item} item 
+     */
+    addItem(item) {
+        let li = document.createElement('li');
+        li.appendChild(item);
+        this.container.appendChild(li);
+    }
+
+    /**
+     * Remove item from menu
+     * 
+     * @param {Integer} id 
+     */
+    delItem(id) {
+        this.elements[id].remove();
+    }
+
+    /**
+     * Add sub section to menu
+     * 
+     * @param {Integer} id 
+     * @param {Menu} menu 
+     */
+    addSubSection(id, menu) {
+        this.elements[id].appendChild(menu);
     }
 }
 
-class SubSection {
+/**
+ * Class NavBar
+ *
+ * @since  1.0.0
+ * @access private
+ */
+class NavBar {
 
-    constructor(index, title) {
-        this.header = document.createElement('li');
-        this.container = document.createElement('ul');
-        this.section = document.createElement('div');
-        this.header.id = 'header-'.concat(index);
-        this.section.id = 'section-'.concat(index);
-        let text = document.createTextNode(title);
-        this.header.appendChild(text);
-        this.header.appendChild(this.section);
-        this.container.appendChild(this.header);
-        return this.container;
+    /**
+     * NavBar constructor
+     * 
+     * @param {Integer} id 
+     */
+    constructor(id) {
+        this.nav = document.createElement('nav');
+        this.nav.setAttribute('class', NAVBAR_CLASS);
+        this.nav.id = 'nav-'.concat(id);
+        this.elements = this.nav.childNodes;
+    }
+
+    /**
+     * Get navbar instance
+     */
+    get() {
+        return this.nav;
+    }
+
+    /**
+     * Get all sections
+     */
+    getSections() {
+        return this.elements;
+    }
+
+    /**
+     * Add section to navbar
+     * 
+     * @param {Section} section 
+     */
+    addSection(section) {
+        this.nav.appendChild(section);
+    }
+
+    /**
+     * Remove section from navbar
+     * 
+     * @param {Integer} id 
+     */
+    delSection(id) {
+        this.elements[id].remove();
     }
 }
+
+
+window.onload = function () {
+
+    var navbar = new NavBar(0);
+    document.getElementById('container').appendChild(navbar.get());
+
+    var section1 = new Section(0);
+    navbar.addSection(section1.get());
+    section1.addItem(new Item(this.itemCounter++, 'Marca1', 'https://marca.com'));
+    section1.addItem(new Item(this.itemCounter++, 'Marca2', 'https://marca.com'));
+    section1.addItem(new Item(this.itemCounter++, 'Marca3', 'https://marca.com'));
+    section1.addItem(new Item(this.itemCounter++, 'Marca4', 'https://marca.com'));
+
+    var subSection = new Section(1);
+    subSection.addItem(new Item(this.itemCounter++, 'AS1', 'https://marca.com'));
+    subSection.addItem(new Item(this.itemCounter++, 'AS2', 'https://marca.com'));
+    subSection.addItem(new Item(this.itemCounter++, 'AS3', 'https://marca.com'));
+    section1.addSubSection(1, subSection.get());
+
+    var subSection2 = new Section(1);
+    subSection2.addItem(new Item(this.itemCounter++, 'AS4', 'https://marca.com'));
+    subSection2.addItem(new Item(this.itemCounter++, 'AS5', 'https://marca.com'));
+    subSection2.addItem(new Item(this.itemCounter++, 'AS6', 'https://marca.com'));
+    subSection.addSubSection(2, subSection2.get());
+
+    var section2 = new Section(2);
+    navbar.addSection(section2.get());
+    section2.addItem(new Item(this.itemCounter++, 'Marca1', 'https://marca.com'));
+    section2.addItem(new Item(this.itemCounter++, 'Marca2', 'https://marca.com'));
+    section2.addItem(new Item(this.itemCounter++, 'Marca3', 'https://marca.com'));
+    section2.addItem(new Item(this.itemCounter++, 'Marca4', 'https://marca.com'));
+};
